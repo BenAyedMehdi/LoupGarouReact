4a672a642200cf3b755f1273c7014f4dfd1f0c92
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2023-04-02 04:41:26 +0200

    generate game ID

--- src/pages/CreateGamePage.js
+++ src/pages/CreateGamePage.js
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
-import { Grid, Container, Typography, Button, TextField} from '@mui/material';
+import { Grid, Container, Typography, Button, TextField } from '@mui/material';
 // components
 import Iconify from '../components/iconify';
+// sections
+import {
+  AppWidgetSummary,
+} from '../sections/@dashboard/app';
+import {
+  TextWidget,
+} from '../sections/new';
 // ----------------------------------------------------------------------
 
 export default function CreateGamePage() {
 
+  const [created, setCreated] = useState(false);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Game created")
+    setCreated(true)
+    }
   return (
     <>
       <Helmet>
@@ -15,28 +30,48 @@ export default function CreateGamePage() {
       </Helmet>
 
       <Container maxWidth="xl">
+        {!created && (
+        <>
         <Typography variant="h4" sx={{ mb: 5 }}>
           Hi, you are the Host, let's create a game!
         </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={3}>
+              <TextField
+                id="outlined-number"
+                label="Number of players"
+                type="number"
+                required
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-          <TextField
-          id="outlined-number"
-          label="Number of players"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
+            <Grid item xs={12} sm={6} md={3}>
+              <Button onClick={handleSubmit} variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+                Create a game
+              </Button>
+            </Grid>
           </Grid>
+        </>
+        )}
+        {created && (
+        <>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Game created! Please join using the game ID
+        </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={3}>
+              <></>
+            </Grid>
 
-          <Grid item xs={12} sm={6} md={3}>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
-            Create a game
-          </Button>
+            <Grid item xs={12} sm={6} md={6}>
+              <TextWidget title="Game ID" value="NXHA" color="warning" icon={'ant-design:windows-filled'} />
+            </Grid>
           </Grid>
-        </Grid>
+        </>
+        )}
       </Container>
     </>
   );
